779c8402e2f1690217f96648ab8d6c08d026dd9d
LinaEngman <150683543+LinaEngman@users.noreply.github.com>
2023-12-22 15:02:54 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -22,35 +22,39 @@ fetch(url)
     // Skapa ett ul-element och ge det en klass
     const eventList = document.createElement("ul");
     eventList.classList.add("eventList");
+      // Hämta alla card-body element
+      const cardBodies = document.querySelectorAll('.card-body');
 
-    // Loopa igenom användarobjekten och skapa li-element för varje
-    eventArray.forEach((event) => {
+      // Loopa igenom användarobjekten och skapa li-element för varje
+      eventArray.forEach((event, index) => {
       // Skapa li-element och fyll det med användarinformation
-      const listItem = document.createElement("li");
-      listItem.innerHTML = `
-        <strong>${event.titel}</strong><br>
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+        ${event.titel}<br>
         Datum: ${event.datum}<br>
         Plats: ${event.plats}<br>
         Tid: ${event.tid}<br>
         <button onclick="handleEdit(${event.id})">Redigera</button>
-        <button onclick="handleDelete(${event.id})">Ta bort</button>
-      `;
-
+        <button onclick="handleDelete(${event.id})">Ta bort</button>`;
+  
       // Lägg till li-elementet i ul-elementet
       eventList.appendChild(listItem);
-    });
-
-    // Lägg till ul-elementet i body eller där du vill ha det i DOM-trädet
-    document.body.appendChild(eventList);
-
-    // Logga resultatet
-    console.log(eventArray);
-    console.log(jsonData);
-  })
-  .catch((error) => {
-    // Hantera eventuella fel under hämtningen
-    console.error("Fetch error:", error);
+  
+      // Lägg till li-elementet i det matchande card-body
+      if (cardBodies[index]) {
+        cardBodies[index].appendChild(listItem);
+      }
   });
+      // Lägg till ul-elementet i body eller där du vill ha det i DOM-trädet
+      document.body.appendChild(eventList);
+      // Logga resultatet
+      console.log(eventArray);
+      console.log(jsonData);
+    })
+    .catch((error) => {
+      // Hantera eventuella fel under hämtningen
+      console.error("Fetch error:", error);
+    });
 
 // funktion för att skapa event
 function addNewEvent(newEvent) {
