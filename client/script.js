29bd429e71f3982b0f066614f914c417abc863f5
Kajsathimon <112699883+Kajsathimon@users.noreply.github.com>
2023-12-22 14:55:25 +0100

    handledelete

--- client/script.js
+++ client/script.js
@@ -23,31 +23,26 @@ fetch(url)
     const eventList = document.createElement("ul");
     eventList.classList.add("eventList");
 
-    // Hämta alla card-body element
-    const cardBodies = document.querySelectorAll('.card-body');
-
     // Loopa igenom användarobjekten och skapa li-element för varje
-    eventArray.forEach((event, index) => {
-    // Skapa li-element och fyll det med användarinformation
+    eventArray.forEach((event) => {
+      // Skapa li-element och fyll det med användarinformation
       const listItem = document.createElement("li");
       listItem.innerHTML = `
-      ${event.titel}<br>
-      Datum: ${event.datum}<br>
-      Plats: ${event.plats}<br>
-      Tid: ${event.tid}<br>
-      <button onclick="handleEdit(${event.id})">Redigera</button>
-      <button onclick="handleDelete(${event.id})">Ta bort</button>`;
+        <strong>${event.titel}</strong><br>
+        Datum: ${event.datum}<br>
+        Plats: ${event.plats}<br>
+        Tid: ${event.tid}<br>
+        <button onclick="handleEdit(${event.id})">Redigera</button>
+        <button onclick="handleDelete(${event.id})">Ta bort</button>
+      `;
 
-    // Lägg till li-elementet i ul-elementet
-    eventList.appendChild(listItem);
+      // Lägg till li-elementet i ul-elementet
+      eventList.appendChild(listItem);
+    });
 
-    // Lägg till li-elementet i det matchande card-body
-    if (cardBodies[index]) {
-      cardBodies[index].appendChild(listItem);
-    }
-});
     // Lägg till ul-elementet i body eller där du vill ha det i DOM-trädet
     document.body.appendChild(eventList);
+
     // Logga resultatet
     console.log(eventArray);
     console.log(jsonData);
@@ -86,12 +81,26 @@ function addNewEvent(newEvent) {
 function handleEdit(id) {
   // Implementera din logik för redigering här
   console.log(`Redigera resurs med ID ${id}`);
+  
 }
 
 // Funktion för att hantera borttagning
 function handleDelete(id) {
-  // Implementera din logik för borttagning här
-  console.log(`Ta bort resurs med ID ${id}`);
+    fetch(`http://localhost:3000/events/${id}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok, status code: ${response.status}`);
+      }
+      // Uppdatera listan och DOM-trädet efter borttagning
+      updateEventList();
+      console.log(`Event med ID ${id} borttaget framgångsrikt`);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+
 }
 
 //--------------- Formulär ---------------
