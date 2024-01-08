68b83c028f90ff498ca677ffdec2a2bf0934f547
asu-01 <alva.sundberg@gmail.com>
2024-01-08 16:11:16 +0100

    .

--- client/script.js
+++ client/script.js
@@ -102,27 +102,30 @@ function handleDelete(event) {
 // Lägg till formulär
 
 
+const form = document.getElementById('myForm');
 
+form.addEventListener('submit', handleAdd);
 
 
 function handleAdd() {
   e.preventDefault();
-
-  const form = document.getElementById('myForm');
 
   const formInput = new FormData(form);
 
   const addEvent = { titel: formInput.get("title"), datum: formInput.get("date"), plats: formInput.get(""), tid: formInput.get("time")}
   const jsonData = JSON.stringify(addEvent);
 
-    console.log(data);
+    console.log(addEvent)
   
 
   fetch(`http://localhost:3000/events/`, {method: 'POST', headers: {'content-type': 'application/json' }, 
   body: jsonData
 })
- .then((response) => response.json())
- .then()
+ .then((response) =>
+  if (!response.ok) {
+
+  }
+ )
 
 }
 
